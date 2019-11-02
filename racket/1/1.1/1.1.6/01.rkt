5b2f55732e8137bd8b2ca6f5a856cdc2bb7d3681
alex <alexsey_89@bk.ru>
2019-11-03 01:05:51 +0300

    1.1.6

--- racket/1/1.1/1.1.6/01.rkt
+++ racket/1/1.1/1.1.6/01.rkt
@@ -39,6 +39,33 @@
           ( (< x 5) (+ 2 3))
     )
 )
+
+; Пример использования cond/else
+
+(define (test2 x y)
+    ( cond ( (> x 10) 40 )
+           (else y)
+
+))
+
+; Пример использования if
+
+(define (test3 x y)
+    (if (= x 30)
+        90
+        y
+    )
+)
+
+; Cond - последовательность выражений которые вычисляются по очереди, if - обязан состоять хотя бы из одного выражения
+
+; Элеметарные предикаты < > =
+; Операции логической композиции and or - особые формы not - обычная процедура
+
+(define (test4 x)
+    ( and (> x 10) (< x 20) (or (= 17) ) )
+)
+
 (check-equal? (abs 1) 1)
 (check-equal? (abs 0) 0)
 (check-equal? (abs -20) 20)
@@ -54,3 +81,11 @@
 (check-equal? (test 7) 10)
 (check-equal? (test 2) 5)
 (check-equal? (test 20) 20)
+
+(check-equal? (test2 7 10) 10)
+(check-equal? (test2 20 6) 40)
+
+(check-equal? (test3 30 2) 90)
+(check-equal? (test3 11 6) 6)
+
+(check-equal? (test4 17) #t )
